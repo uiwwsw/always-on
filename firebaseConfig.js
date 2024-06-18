4455555c31c5a87a5dfa3b4ac4788872f2973630
uiwwsw <uiwwsw@icloud.com>
2024-06-18 11:59:50 +0900

    feat: save

---
+++ firebaseConfig.js
@@ -0,0 +1,23 @@
+import { initializeApp } from "firebase/app";
+
+// Optionally import the services that you want to use
+// import {...} from "firebase/auth";
+// import {...} from "firebase/database";
+// import {...} from "firebase/firestore";
+// import {...} from "firebase/functions";
+// import {...} from "firebase/storage";
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyD3YKB4Xp6VJm2Ut2JnlDl4irdiDabQTx4",
+  authDomain: "always-on-41404.firebaseapp.com",
+  projectId: "always-on-41404",
+  storageBucket: "always-on-41404.appspot.com",
+  messagingSenderId: "947668522925",
+  appId: "1:947668522925:web:93bedeb7d22785c349b324",
+  measurementId: "G-053YWRPK79",
+};
+
+const app = initializeApp(firebaseConfig);
+// For more information on how to access Firebase in your project,
+// see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
