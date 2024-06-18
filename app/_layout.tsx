6aaded899e0bd1127b73078e057076fe5163b724
uiwwsw <uiwwsw@icloud.com>
2024-06-18 15:41:45 +0900

    save

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,16 @@
 import { Stack } from 'expo-router';
 import { NativeWindStyleSheet } from 'nativewind';
+import { View } from 'react-native';
 
 NativeWindStyleSheet.setOutput({
   default: 'native',
 });
 
 export default function Layout() {
-  return <Stack />;
+  return (
+    <>
+      <Stack />
+      <View className="flex h-10 w-10 bg-slate-800" />
+    </>
+  );
 }
--- src/templates/Welcome.tsx
+++ src/templates/Welcome.tsx
@@ -2,7 +2,7 @@ import { Text, View } from 'react-native';
 
 const Welcome = () => (
   <View className="flex-1 items-center justify-center">
-    <Text className="text-red-700">
+    <Text className="text-blue-700">
       Open up App/ to start working on your app!
     </Text>
   </View>
